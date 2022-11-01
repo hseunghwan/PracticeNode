588ed44db7b60d80b9f4f9c948857214b2a8ed2c
hseunghwan <hsh9298080@naver.com>
2022-11-01 22:32:15 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express') //다운받은 express 모듈을 가져옴
 const app = express() //express app생성
 const port = 5000  
+
+const config = require('./config/key')
 //const bodyParser = require('body-parser'); //req.body 에 정보가 담길 수 있게 해줌 , 
 //업데이트 후 express에 기본적을 포함된다고 함
 const { User } = require("./models/User");
@@ -15,13 +17,13 @@ app.use(express.urlencoded({extended: true}));
 
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://seunghwan:qaqg3200@practicenode.qmip1z4.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     //useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false // 몽구스 버전 6이상부터는 디폴트 설정되어있어 옵션 추가시 오류 발생함
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {   //루트 디렉토리에 hello world 츨력
-  res.send('Hello World!~~야호')
+  res.send('Hello World!~~야호오')
 })
 
 app.post('/register', (req, res) => {
